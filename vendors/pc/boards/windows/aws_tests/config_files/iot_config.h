3c2424d74e1e6c5e38c02e1fdbace74798147a55
Sarena Meas <sarem@amazon.com>
2019-09-30 15:09:41 -0700

    turn on library logging.

--- vendors/pc/boards/windows/aws_tests/config_files/iot_config.h
+++ vendors/pc/boards/windows/aws_tests/config_files/iot_config.h
@@ -31,4 +31,7 @@
 /* Include the common configuration file for FreeRTOS. */
 #include "iot_config_common.h"
 
+#define IOT_LOG_LEVEL_NETWORK                   IOT_LOG_DEBUG
+#define IOT_LOG_LEVEL_HTTPS                     IOT_LOG_DEBUG
+
 #endif /* ifndef IOT_CONFIG_H_ */
